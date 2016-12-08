2de543c443ad6f6c09ab7905c97f6b508c7faf7d
dianyingx007 <dianyingx007@163.com>
2016-12-08 20:56:04 +0800

    encapsulated as function

--- js/3d-hover.js
+++ js/3d-hover.js
@@ -1,13 +1,25 @@
-$('#test').on('mousemove',function(e){
-	var offset = $('#test').offset();
-	var x = e.pageX - offset.left;
-	var y = e.pageY - offset.top;
-	var centerX = $('#test').outerWidth()/2;
-	var centerY = $('#test').outerHeight()/2;
-	var percentX = (x-centerX)/centerX;
-	var percentY = (y-centerY)/centerY;
-	var deg = 10;/*最大旋转角度*/
-	$('#banner').css({
-		transform:'rotateX('+deg*-percentY+'deg)'+' rotateY('+deg*percentX+'deg)'
-	});
-});
+/*base on jquery*/
+$.fn.hover3d = function(option){
+	var defaults = {
+		outer:$.noop,
+		inner:$(this),
+		deg:10
+	};
+	var params = $.extend({}, defaults, option||{});
+	if(params["outer"]!==$.noop){
+		params["outer"].on('mousemove',function(e){
+			var offset = params["outer"].offset();
+			var x = e.pageX - offset.left;
+			var y = e.pageY - offset.top;
+			var centerX = params["outer"].outerWidth()/2;
+			var centerY = params["outer"].outerHeight()/2;
+			var percentX = (x-centerX)/centerX;
+			var percentY = (y-centerY)/centerY;
+			var deg = 10;/*最大旋转角度*/
+			params["inner"].css({
+				transform:'rotateX('+deg*-percentY+'deg)'+' rotateY('+deg*percentX+'deg)'
+			});
+		});
+	}
+};
+
